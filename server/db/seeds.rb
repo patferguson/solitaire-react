3dd0d27ea23cacf5273c04cb621723dffa2e3da0
Patrick Ferguson <patrick.l.ferguson@outlook.com.au>
2016-12-31 13:16:43 +1100

    Add WIP seeds.rb file for generating users, waiting on User model to be added.

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Only create an admin user for local development environments to speed up setup
+# TODO: Consider adding a version of this command for :production that pulls this default initial admin user password from an environment variable to speed up production setup?
+if Rails.env.to_sym == :development
+  #User.create!(display_name: "Admin", name: "Admin User", email: "admin@example.com", is_admin: true, password: "Admin123", password_confirmation: "Admin123")
+end
