34b4008143833ca0297f93d82a692689abeec325
Patrick Ferguson <patrick.l.ferguson@outlook.com.au>
2017-01-02 12:06:09 +1100

    Add basic smoke tests for all React pages/components.

--- client/src/pages/components/APIVersion/APIVersion.test.js
+++ client/src/pages/components/APIVersion/APIVersion.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import APIVersion from './APIVersion';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<APIVersion />, div);
 });
--- client/src/pages/components/Ping/Ping.test.js
+++ client/src/pages/components/Ping/Ping.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Ping from './Ping';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Ping />, div);
 });
